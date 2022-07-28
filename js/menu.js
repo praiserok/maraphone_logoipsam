6cf1888f87cd88ecfae588c5757f44de2a544af6
praiserok <praiser@rambler.ru>
2022-07-28 17:21:08 +0300

    добавив для цін зміну стилів

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,19 @@ menuBtn.addEventListener('click', function () {
   menuBtn.classList.toggle('active');
   menu.classList.toggle('active');
 })
+
+
+let price = document.querySelector('.price');
+let paket = document.querySelectorAll('.paket');
+
+price.addEventListener('click', (event) => {
+  let why = event.target
+  if (why.classList.contains('paket')) {
+    for (var i = 0; i < paket.length; i++) {
+      if (paket[i].classList.contains('activePrice')) {
+        paket[i].classList.remove('activePrice')
+      }
+    }
+    why.classList.add('activePrice')
+  }
+})
